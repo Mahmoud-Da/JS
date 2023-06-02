5415bc46c0b0289313e2abbf2a7b21650c1be7eb
cham1235 <hana171819@gmail.com>
2023-06-03 01:06:17 +0900

    練習0603

---
+++ self_study/javascript/study_0603.js
@@ -0,0 +1,51 @@
+class Staffs {
+  constructor(name, age, technique, cocktail, play, time) {
+    this.name = name;
+    this.age = age;
+    this.technique = technique;
+    this.cocktail = cocktail;
+    this.play = play;
+    this.time = time;
+    console.log(`名前は${this.name}です`);
+    console.log(`年齢は${this.age}です`);
+    console.log(`専門は${this.technique}です`);
+    console.log(`飲み物は${this.cocktail}です`);
+    console.log(`趣味は${this.play}です`);
+    console.log(`回数は${this.time}です`);
+  }
+  greet() {
+    console.log("初めまして");
+    console.log(`
+        私は${this.name}です。
+        今年${this.age}、${this.technique}専門で
+        得意飲み物は${this.cocktail}です。`);
+    console.log("自分の仕事が大好きです。");
+  }
+  sports() {
+    console.log(`さらに、${this.play}をする。`);
+    console.log(`毎月、最低に${this.time}回を遊びに行きます。`);
+  }
+}
+const ken = new Staffs(
+  "Ken",
+  25,
+  "バーテン",
+  "カクテル ネグローニ",
+  "テニス",
+  "3"
+);
+ken.greet();
+ken.sports();
+console.log(ken);
+
+const lili = new Staffs(
+  "Lili",
+  20,
+  "バーテン",
+  "カクテル ブラッド&サンド",
+  "チェス",
+  "4"
+);
+lili.greet();
+lili.sports();
+console.log(lili);
