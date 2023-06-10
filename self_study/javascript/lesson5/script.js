94bb30d09a62045426d1520a6c5f76625e32b319
cham1235 <hana171819@gmail.com>
2023-06-10 09:33:47 +0900

    復習lesson5_6

--- self_study/javascript/lesson5/script.js
+++ self_study/javascript/lesson5/script.js
@@ -2,5 +2,9 @@ import Dog from "./dog";
 const dog = new Dog("レオ", 4, "チワワ");
 dog.info();
 
-import dog from "./dogData";
+import { dog1, dog2 } from "./dogData";
 dog.info();
+console.log("---------");
+dog1.info();
+console.log("---------");
+dog2.info();
